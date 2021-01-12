37ca5c2706726d86c0385973e92c4049159619ac
Sergei Sindeev <sinsst30w@gmail.com>
2021-01-12 22:41:56 +0400

    substance class added

---
+++ src/main/java/com/example/Chemistry/model/SubstanceClass.java
@@ -0,0 +1,12 @@
+package com.example.Chemistry.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter@Setter
+public class SubstanceClass {
+    private int id;
+    private String name;
+}
