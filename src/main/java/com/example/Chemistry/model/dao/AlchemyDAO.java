5592c9ae00e47f4994632de6e73f0265ce4c9b99
Sergei Sindeev <sinsst30w@gmail.com>
2021-01-13 20:05:40 +0400

    added main page, refactoring, update methods added

--- src/main/java/com/example/Chemistry/model/dao/AlchemyDAO.java
+++ src/main/java/com/example/Chemistry/model/dao/AlchemyDAO.java
@@ -3,6 +3,7 @@ package com.example.Chemistry.model.dao;
 import com.example.Chemistry.model.Formula;
 import com.example.Chemistry.model.Ion;
 import com.example.Chemistry.model.Substance;
+import com.example.Chemistry.model.SubstanceClass;
 import com.example.Chemistry.model.dao.interfaces.IAlchemyDAO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,9 +27,16 @@ public class AlchemyDAO implements IAlchemyDAO {
     //queries
     private static final String selectQuery = "SELECT * FROM ? WHERE id = ?";
     private static final String deleteQuery = "DELETE FROM ? WHERE id = ?";
+
     private static final String substanceInsertQuery = "INSERT INTO substances VALUES(?, ?, ?)";
     private static final String formulaInsertQuery = "INSERT INTO formulas VALUES(?, ?, ?, ?)";
     private static final String ionInsertQuery = "INSERT INTO ions VALUES(?, ?, ?, ?)";
+    private static final String classInsertQuery = "INSERT INTO classes VALUES(?)";
+
+    private static final String substanceUpdateQuery = "UPDATE substances SET formulaId = ?, classId = ? WHERE id = ?;";
+    private static final String formulaUpdateQuery = "UPDATE formulas SET cationId = ?, anionId = ?, notation = ? WHERE id = ?;";
+    private static final String ionUpdateQuery = "UPDATE ions SET valence = ?, notation = ?, type = ? WHERE id = ?;";
+    private static final String classUpdateQuery = "UPDATE classes SET name = ? WHERE id = ?;";
 
     @Override
     public boolean addSubstance(Substance substance) {
@@ -75,6 +83,20 @@ public class AlchemyDAO implements IAlchemyDAO {
         }
         log.info("Ion {} added", ion.getNotation());
         return isIonAdded;
+    }
+
+    @Override
+    public boolean addSubstanceClass(SubstanceClass substanceClass) {
+        boolean isAdded = false;
+        try (Connection connection = null;
+             PreparedStatement statement = connection.prepareStatement(classInsertQuery)) {
+            statement.setString(2, substanceClass.getName());
+            isAdded = statement.execute();
+        } catch (SQLException ex) {
+            log.error("Failed to add Ion!");
+        }
+        log.info("Ion {} added", substanceClass.getName());
+        return isAdded;
     }
 
     @Override
@@ -166,6 +188,69 @@ public class AlchemyDAO implements IAlchemyDAO {
         }
         if(isDeleted) log.info("Entity #{} deleted from {} successfully", id, tableName);
         return isDeleted;
+    }
+
+    // update
+
+    @Override
+    public boolean updateSubstance(Substance substance) {
+        boolean isUpdated = false;
+        try (Connection connection = null;
+             PreparedStatement statement = connection.prepareStatement(substanceUpdateQuery)) {
+            statement.setString(2, substance.getFormula());
+            statement.setInt(3, substance.getClassId());
+            isUpdated = statement.execute();
+        } catch (SQLException ex) {
+            log.error("Failed to add substance!");
+        }
+        log.info("Substance {} added", substance.getFormula());
+        return isUpdated;
+    }
+
+    @Override
+    public boolean updateFormula(Formula formula) {
+        boolean isUpdated = false;
+        try (Connection connection = null;
+             PreparedStatement statement = connection.prepareStatement(formulaUpdateQuery)) {
+            statement.setInt(2, formula.getAnionId());
+            statement.setInt(3, formula.getCationId());
+            statement.setString(4, formula.getNotation());
+            isUpdated = statement.execute();
+        } catch (SQLException ex) {
+            log.error("Failed to add formula!");
+        }
+        log.info("Formula {} added", formula.getNotation());
+        return isUpdated;
+    }
+
+    @Override
+    public boolean updateIon(Ion ion) {
+        boolean isUpdated = false;
+        try (Connection connection = null;
+             PreparedStatement statement = connection.prepareStatement(ionUpdateQuery)) {
+            statement.setInt(2, ion.getValence());
+            statement.setString(3, ion.getNotation());
+            statement.setString(4, ion.getType().name());
+            isUpdated = statement.execute();
+        } catch (SQLException ex) {
+            log.error("Failed to add Ion!");
+        }
+        log.info("Ion {} added", ion.getNotation());
+        return isUpdated;
+    }
+
+    @Override
+    public boolean updateSubstanceClass(SubstanceClass substanceClass) {
+        boolean isUpdated = false;
+        try (Connection connection = null;
+             PreparedStatement statement = connection.prepareStatement(classUpdateQuery)) {
+            statement.setString(2, substanceClass.getName());
+            isUpdated = statement.execute();
+        } catch (SQLException ex) {
+            log.error("Failed to add Ion!");
+        }
+        log.info("Ion {} added", substanceClass.getName());
+        return isUpdated;
     }
 
 
--- src/main/java/com/example/Chemistry/model/dao/interfaces/IAlchemyDAO.java
+++ src/main/java/com/example/Chemistry/model/dao/interfaces/IAlchemyDAO.java
@@ -3,6 +3,7 @@ package com.example.Chemistry.model.dao.interfaces;
 import com.example.Chemistry.model.Formula;
 import com.example.Chemistry.model.Ion;
 import com.example.Chemistry.model.Substance;
+import com.example.Chemistry.model.SubstanceClass;
 
 public interface IAlchemyDAO {
     //select queries
@@ -13,7 +14,13 @@ public interface IAlchemyDAO {
     boolean addSubstance(Substance substance);
     boolean addFormula(Formula formula);
     boolean addIon(Ion ion);
+    boolean addSubstanceClass(SubstanceClass substanceClass);
     //delete queries
     boolean deleteEntityById(String entity, int entityId);
     //update queries
+    boolean updateSubstance(Substance substance);
+    boolean updateFormula(Formula formula);
+    boolean updateIon(Ion ion);
+    boolean updateSubstanceClass(SubstanceClass substanceClass);
+
 }
