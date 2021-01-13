83edbec6c64f85631940a018125672dd9d6e1ac1
Sergei Sindeev <sinsst30w@gmail.com>
2021-01-13 20:06:21 +0400

    обновлён контроллер

--- src/main/java/com/example/Chemistry/controller/MainController.java
+++ src/main/java/com/example/Chemistry/controller/MainController.java
@@ -11,56 +11,33 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 
 @Controller
 public class MainController {
-    private static List<Person> persons = new ArrayList<>();
-    static {
-        persons.add(new Person("Bill", "Gates"));
-        persons.add(new Person("Gill", "Bates"));
-    }
-
-    @Value("${welcome.message}")
-    private String message;
-    @Value("${error.message}")
-    private String errorMessage;
+//    @Value("${welcome.message}")
+//    private String message;
+//    @Value("${error.message}")
+//    private String errorMessage;
 
     @GetMapping(value={"/","/index"})
     public String index(Model model){
-        model.addAttribute("message", message);
+//        model.addAttribute("message", message);
         return "index";
     }
 
-    @GetMapping("/personList")
-    public String personList(Model model){
-        model.addAttribute("persons", persons);
-        return "personList";
-    }
-
-    @GetMapping("/addPerson")
-    public String showAddPersonPage(Model model){
-        PersonForm personForm = new PersonForm();
-        model.addAttribute("personForm", personForm);
-        return "addPerson";
-    }
-
-    @PostMapping("/addPerson")
-    public String savePerson(Model model, @ModelAttribute("personForm") PersonForm personForm){
-        String firstName = personForm.getFirstName();
-        String lastName = personForm.getLastName();
-        if(!firstName.isBlank() && !lastName.isBlank()){
-            Person newPerson = new Person(firstName,lastName);
-            persons.add(newPerson);
-            return "redirect:/personList";
-        }
-        model.addAttribute("errorMessage", errorMessage);
-        return "addPerson";
-    }
-
+//    @PostMapping("/addPerson")
+//    public String savePerson(Model model, @ModelAttribute("personForm") PersonForm personForm){
+//        String firstName = personForm.getFirstName();
+//        String lastName = personForm.getLastName();
+//        if(!firstName.isBlank() && !lastName.isBlank()){
+//            Person newPerson = new Person(firstName,lastName);
+////            persons.add(newPerson);
+//            return "redirect:/personList";
+//        }
+////        model.addAttribute("errorMessage", errorMessage);
+//        return "addPerson";
+//    }
 }
 
