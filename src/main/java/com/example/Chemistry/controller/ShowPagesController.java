8d76c027afa066a8c1e98628f527e4761de87bbb
Sergei Sindeev <sinsst30w@gmail.com>
2021-01-15 18:33:53 +0400

    добавлено выражение переменной списка веществ, улучшена разметка

--- src/main/java/com/example/Chemistry/controller/ShowPagesController.java
+++ src/main/java/com/example/Chemistry/controller/ShowPagesController.java
@@ -7,52 +7,65 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 class IonView {
-    public String formulaText = "111";
-    public String notationText = "222";
-    public String classText = "333";
+    public String typeText = "type";
+    public String notationText = "notation";
+    public String valenceText = "valence";
 }
 
 class ClassView {
-    public String formulaText = "111";
-    public String notationText = "222";
-    public String classText = "333";
+    public String nameText = "name";
 }
 
 class SubstanceView {
-    public String formulaText = "111";
-    public String notationText = "222";
-    public String classText = "333";
+    public String formulaText = "formula";
+    public String notationText = "notation";
+    public String classText = "class";
 }
 
 @Controller
 public class ShowPagesController {
-    @GetMapping(value={"/","/index"})
-    public String index(Model model){
+    @GetMapping(value = {"/", "/index"})
+    public String index(Model model) {
         model.addAttribute("substances", List.of(new SubstanceView(), new SubstanceView()));
 
         return "index";
     }
 
     @GetMapping(value = {"/login"})
-    public String login(){
+    public String login(Model model) {
         return "login";
     }
 
     @GetMapping(value = {"/classes/main"})
-    public String showClasses(){return "classes/main";}
+    public String showClasses(Model model) {
+        model.addAttribute("classes", List.of(new ClassView(), new ClassView()));
+
+        return "classes/main";
+    }
+
     @GetMapping(value = {"/ions/main"})
-    public String showIons(){return "ions/main";}
+    public String showIons(Model model) {
+        model.addAttribute("ions", List.of(new IonView(), new IonView()));
+
+        return "ions/main";
+    }
 
     @GetMapping(value = {"classes/create"})
-    public String showCreateClass(){
+    public String showCreateClass(Model model) {
         return "classes/create";
     }
+
     @GetMapping(value = {"/ions/create"})
-    public String showCreateIon(){
+    public String showCreateIon(Model model) {
         return "ions/create";
     }
+
     @GetMapping(value = {"/substances/create"})
-    public String showCreateSubstance(){
+    public String showCreateSubstance(Model model) {
+        model.addAttribute("classes", List.of(new ClassView(), new ClassView()));
+        model.addAttribute("anions", List.of(new IonView(), new IonView()));
+        model.addAttribute("cations", List.of(new IonView(), new IonView()));
+
         return "substances/create";
     }
 }
