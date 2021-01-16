1c8eff6c00818a8d435b1b2e79e7a837116a55b2
Sergei Sindeev <sinsst30w@gmail.com>
2021-01-16 20:06:16 +0400

    реализовано создание любых записей

--- src/main/java/com/example/Chemistry/view/CreateSubstanceForm.java
+++ src/main/java/com/example/Chemistry/view/CreateSubstanceForm.java
@@ -1,12 +1,25 @@
 package com.example.Chemistry.view;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter@Setter
+@Getter
+@Setter
+@Builder
 public class CreateSubstanceForm {
     private int classId;
     private int cationId;
     private int anionId;
     private String notation;
+
+    public CreateSubstanceForm() {
+    }
+
+    public CreateSubstanceForm(int classId, int cationId, int anionId, String notation) {
+        this.classId = classId;
+        this.cationId = cationId;
+        this.anionId = anionId;
+        this.notation = notation;
+    }
 }
