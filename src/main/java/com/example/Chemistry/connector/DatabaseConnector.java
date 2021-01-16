a4fa7cadb06ab9dce514cde8422ce1eb7dcaf58e
spat!alM0use <sbw@bk.ru>
2021-01-16 11:31:30 +0400

    обновлён коннектор

--- src/main/java/com/example/Chemistry/connector/DatabaseConnector.java
+++ src/main/java/com/example/Chemistry/connector/DatabaseConnector.java
@@ -11,9 +11,8 @@ public class DatabaseConnector {
     private static Connection connection;
     private static final String dbURL = "jdbc:hsqldb:file:C:/Users/Сергей/IdeaProjects/Chemistry/src/main/resources/database/ChemicalDB.db";
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+    public static Connection getConnection() throws SQLException {
         if(connection != null) return connection;
-        Class.forName("org.hsqldb.jdbc.JDBCDriver");
         connection = DriverManager.getConnection(dbURL);
         return connection;
     }
